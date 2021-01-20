987c084b7dd1bf617a73612d4aa1df7163809234
Doug K <doug.kerzner@gmail.com>
2021-01-20 22:10:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,11 +2,13 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   master_hash = {}
-  data.each do |key, value| 
-    value.each do |
+  data.each { |outer_key, outer_value| 
+    outer_value.each { |inner_key, outer_key|
+      
+    
     if value.class == String  
       master_hash[key] = value
     end 
 # master_hash
-  end 
+  } }
 end
