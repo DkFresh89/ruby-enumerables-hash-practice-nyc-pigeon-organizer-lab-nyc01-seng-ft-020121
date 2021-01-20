123e30d18ef1da0182377a19fed03bafeb2c7f0c
Doug K <doug.kerzner@gmail.com>
2021-01-20 23:02:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
    #binding.pry 
    value.each do |inner_key, names|
      names.each do |name|
-       if !final_array[name] do 
+       if !final_array[name]
          
        binding.pry
      end 
