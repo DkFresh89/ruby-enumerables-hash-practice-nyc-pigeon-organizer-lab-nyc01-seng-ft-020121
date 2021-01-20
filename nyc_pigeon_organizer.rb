3f6cf2f03ed05b453d15914123c36c72e83d475f
Doug K <doug.kerzner@gmail.com>
2021-01-20 22:31:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,6 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  master_hash = {}
-  data.each do |outer_key, outer_value| 
-    outer_value.each do |inner_key, inner_value|
-      inner_value.each do |deep_key, deep_value|
-        master_hash[deep_value] = {}
-      
-    
-   # if value.class == String  
-    #  master_hash[key] = value
-    end 
-# master_hash
-end 
-end 
+ 
+binding.pry 
 end
