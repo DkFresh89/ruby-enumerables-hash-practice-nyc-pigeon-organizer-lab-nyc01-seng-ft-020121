9657fa7fed80c6704e0e9a63497cf303e49e2b21
Doug K <doug.kerzner@gmail.com>
2021-01-20 21:53:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   master_hash = {}
   data.each do |key, value| 
     if value == String  
-      master_hash[key] = name 
+      master_hash[key] = value
     end 
 # master_hash
   end 
